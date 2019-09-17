4bc9815bf195136d95ddcbc439042099344cdefc
Areum Jo <areumjo1@gmail.com>
2019-09-17 14:39:36 -0700

    Added new get-endpoint for getCourseById

--- after-school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ after-school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -1,7 +1,11 @@
 package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.CourseService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -23,6 +27,12 @@ public class CourseController
     // localhost:2019/courses/courses
     // localhost:2019/courses/courses/?page=0&size=1 <== showing only one datapoint (courseid = 1)
     // localhost:2019/courses/courses/?sort=students,desc&sort=coursename
+    @ApiOperation(value = "Returns all 3 courses on each page", response = Course.class, responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query", value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query", value = "Number of records per page"),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query", value = "Sorting criteria in the format: property(,asc|desc). " + "Default sort order is ascending. " + "Multiple sort criteria are supported.")
+    })
     @GetMapping(value = "/courses", produces = {"application/json"})
     public ResponseEntity<?> listAllCourses(
             @PageableDefault(page = 0,
@@ -39,6 +49,15 @@ public class CourseController
     {
         List<Course> myCourses = courseService.findAll(Pageable.unpaged());
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
+    }
+
+    // localhost:2019/course/course/{courseid}
+    @GetMapping(value = "/course/{courseid}",
+            produces = {"application/json"})
+    public ResponseEntity<?> getCourseById(@PathVariable long courseid)
+    {
+        Course cour = courseService.findCourseById(courseid);
+        return new ResponseEntity<>(cour, HttpStatus.OK);
     }
 
     @GetMapping(value = "/studcount", produces = {"application/json"})
--- after-school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ after-school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -44,4 +44,10 @@ public class CourseServiceImpl implements CourseService
             throw new EntityNotFoundException(Long.toString(id));
         }
     }
+
+    @Override
+    public Course findCourseById(long id) {
+        return courserepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+    }
 }
